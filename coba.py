7bdfeb053a95470999d9a1182e20882560b0cf1d
anasujicoba <122507819+anasujicoba@users.noreply.github.com>
2023-01-12 12:12:03 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
 st.write("""
-#Luas segitiga sama jenjang
-sjdakskafakfka fhdafjadfkad faudbfadjfa dfajdfdafndaf adjfkfds """)
+# Luas segitiga sama jenjang
+sjdakskafakfka fhdafjadfkad faudbfadjfa dfajdfdafndaf adjfkfds
+""")
+alas = st.number_input("masukkan alas", 0)
+tinggi = st.number_input("masukkan tinggi", 0)
+
+hitung =st.button("hitung Luar")
+
+if hitung:
+    luas =0.5 * alas * tinggi
+    st.success(f"luasnya adalah{luas}")
